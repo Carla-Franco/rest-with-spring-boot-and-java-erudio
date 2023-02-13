5d9fa4ad72818ffa2a81c07ae9ae78a242e8db6a
cribeiro.ifce <carla.ribeiro@darmlabs.ifce.edu.br>
2023-02-13 17:01:02 -0300

    Seção 14

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Domingos Martins', 'Carla', 'Femi', 'Franco'),
+	(2, 'Domingos Martins', 'Rondonito', 'Male', 'Medeiros Franco'),
+	(5, 'Marechal Floriano', 'Raphael', 'Male', 'Ferreira'),
+	(7, 'Domingos Martins', 'Clara', 'Female', 'Franco'),
+	(8, 'Lajinha', 'João', 'Male', 'Souza');
